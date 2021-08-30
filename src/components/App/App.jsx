61819640bace8a75af648d8f368954041e1bd5fd
pavlos noulis <pavlos.noulis@gmail.cqm>
2021-08-30 14:03:15 +0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import Layout from "./Layout";
+
+export
+const
+App = () => {
+  return (
+    <Layout></Layout>
+  );
+};
+
---
+++ src/components/App/Layout.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const
+Layout = styled.article`
+flex: 1;
+min-height: 100vh;
+display: grid;
+grid-template-columns: minmax(auto, 350px) 1fr;
+grid-template-rows: auto 1fr;
+
+header {
+grid-column: 1/3;
+grid-row: 1/2;
+
+}
+
+aside {
+grid-column: 1/2;
+grid-row: 2/3;
+
+}
+
+main {
+grid-column: 2/3;
+grid-row: 2/3;
+}
+`;
+
+export default Layout;
