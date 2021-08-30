8533c91edf34e60bffa2c943a4b6e55d8c3b6b31
pavlos noulis <pavlos.noulis@gmail.cqm>
2021-08-30 14:14:11 +0300

    app layout ready

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,12 @@ export
 const
 App = () => {
   return (
-    <Layout></Layout>
+    <Layout>
+      <header>"replace with header"</header>
+      <aside>"replace with aside"</aside>
+      <main id="main-content">main content</main>
+      <footer>"replace with footer"</footer>
+    </Layout>
   );
 };
 
--- src/components/App/Layout.jsx
+++ src/components/App/Layout.jsx
@@ -6,7 +6,7 @@ flex: 1;
 min-height: 100vh;
 display: grid;
 grid-template-columns: minmax(auto, 350px) 1fr;
-grid-template-rows: auto 1fr;
+grid-template-rows: auto 1fr auto;
 
 header {
 grid-column: 1/3;
@@ -23,6 +23,11 @@ grid-row: 2/3;
 main {
 grid-column: 2/3;
 grid-row: 2/3;
+}
+
+footer {
+grid-column: 1/3;
+grid-row: 3/4;
 }
 `;
 
